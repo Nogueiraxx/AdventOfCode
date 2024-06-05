9d6a58d791511a47bbd7bc6f2f9402c39958cb45
NogueiraZx <nogueira.neto1106@gmail.com>
2024-06-05 04:36:50 -0300

    Solução Day One Parte 1

--- DAY 1/DigitsFromStrings.py
+++ DAY 1/DigitsFromStrings.py
@@ -1,37 +1,35 @@
-#! Funciona para chaves que contem apenas números, o input do desafio envolve strings que contem numero por extenso ex: five3 e que nesse caso isso representa 53.
-# def BuscarDigito(frases):
-#     soma = 0
-
-#     for frase in frases:
-#         for i in range(len(frase)):
-#             character = frase[i]
-#             if character.isnumeric():
-#                 soma += int(character)
-#                 break
-        
-#     return int(soma)
+#! Funciona para chaves que contem apenas números, 
+#o input do desafio envolve strings que contem numero por extenso ex: 
+#five3 e que nesse caso isso representa 53.
+def BuscarDigito(frases):
+    for i in range(len(frases)):
+        character = frases[i]
+        if character.isdigit():
+            soma = character
+            return soma
 
 
-# def abrirDoc():
-#     with open('CalibrationDoc.txt') as arq:
-#         linhas = arq.readlines()
-#     keys = []
-#     cont = 0
+def abrirDoc():
+    with open('DAY 1\CalibrationDoc.txt') as arq:
+        linhas = arq.readlines()
+    keys = []
 
-#     for i in range(len(linhas)):
-#         charact = linhas[i]
-#         keys.append(charact)
+    for i in range(len(linhas)):
+        charact = linhas[i]
+        keys.append(charact)
+    return keys
 
-#     return keys
 
-# chaves = abrirDoc()
-# somatorio = 0
-# for key in chaves:
-#     frases = [key]
-#     frases.append(key[::-1])
-#     somatorio += BuscarDigito(frases[0])
-#     somatorio += BuscarDigito(frases[1])
+chaves = abrirDoc()
+truncado = ''
+somatorio = 0
+for key in chaves:
+    frases = [key]
+    frases.append(key[::-1])
+    truncado = BuscarDigito(frases[0]) + BuscarDigito(frases[1])
+    somatorio += int(truncado)
 
-# print(somatorio)
+print(somatorio)
+
 
 
